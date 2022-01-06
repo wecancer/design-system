21f65538a3d917c7da63ce062157433f5402959a
Thales <thales.caricati@gmail.com>
2022-01-06 16:46:41 -0300

    configure the storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  framework: '@storybook/react',
+  babel: async (options) => {
+    options.plugins.push('babel-plugin-inline-react-svg')
+    return options
+  },
+  core: {
+    builder: 'webpack5',
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import React from 'react'
+import {ThemeProvider} from 'styled-components'
+import WecancerProvider from '../src/WecancerProvider'
+
+export const decorators = [
+  (Story) => (
+    <WecancerProvider>
+      <Story />
+    </WecancerProvider>
+  )
+]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
