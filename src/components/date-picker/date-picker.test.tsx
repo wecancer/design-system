e5ba64ca2322b0b483151ac598f65bc27736b1da
Thales <thales.caricati@gmail.com>
2022-04-04 16:56:29 -0300

    update snapshots

--- src/components/date-picker/date-picker.test.tsx
+++ src/components/date-picker/date-picker.test.tsx
@@ -130,7 +130,11 @@ describe('Snapshot <DatePicker />', () => {
   it('should take a snapshot', () => {
     const { container } = render(
       <>
-        <DatePicker fromYear={2022} toYear={2025} />
+        <DatePicker
+          fromYear={2022}
+          toYear={2025}
+          value={new Date('2012-12-12')}
+        />
       </>,
     )
     const input = container.querySelector('input[type="text"]') as HTMLElement
