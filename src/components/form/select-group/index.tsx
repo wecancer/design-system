5f7090af99ebe740e3ea6934795b01aa44391481
Gustavo Barreto <gbarreto.alves2@gmail.com>
2022-08-09 16:23:05 -0300

    feat: adjusting the SelectGroup to show 'all' when all options were selected

--- src/components/form/select-group/index.tsx
+++ src/components/form/select-group/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MultiValue } from 'react-select'
 
 import GenericSelect from '../select/generic-select'
@@ -12,6 +12,7 @@ type ChangeParams = {
 
 export type Props = Omit<SelectProps, 'value' | 'onChange'> & {
   value?: SelectOptions
+  allValues: SelectOptions
   hasSelectAll?: boolean
   onChange(args: ChangeParams): void
 }
@@ -21,6 +22,7 @@ const SelectGroup = ({
   label,
   onChange,
   required,
+  allValues,
   className,
   onLoadMore,
   onScrollEnd,
@@ -30,7 +32,12 @@ const SelectGroup = ({
 }: Props) => {
   const t = useTranslation()
 
-  const isAllSelected = options.length === value?.length
+  const [verification, setVerification] = useState(false)
+
+  const isAllSelected = onLoadMore?.length
+    ? allValues.length === value?.length || verification
+    : options.length === value?.length || verification
+
   const selectAllOption = {
     label: t(isAllSelected ? 'Unselect all' : 'Select all'),
     value: '__wcds_select-all',
@@ -38,23 +45,38 @@ const SelectGroup = ({
 
   const opts = hasSelectAll ? [selectAllOption, ...options] : options
 
+  const newLabel = { label: t('All'), value: '' }
+
   return (
     <GenericSelect
       isMulti
       label={label}
-      value={value}
-      options={opts}
+      value={isAllSelected ? newLabel : value}
+      options={isAllSelected ? [] : opts}
       required={required}
       className={className}
-      onLoadMore={onLoadMore}
+      onLoadMore={!verification ? onLoadMore : undefined}
       onScrollEnd={onScrollEnd}
       closeMenuOnSelect={false}
       isMenuListLoading={isMenuListLoading}
       onChange={(val: MultiValue<SelectOption>, action) => {
         const newOptionValue = val as SelectOptions
         if (action.option?.value === selectAllOption.value) {
-          onChange({ value: isAllSelected ? [] : options })
+          setVerification(true)
+          onChange({ value: isAllSelected ? [] : allValues })
           return
+        }
+        if (isAllSelected) {
+          onChange({ value: isAllSelected ? [] : options })
+        }
+        if (action.action === 'clear') {
+          setVerification(false)
+        }
+        if (value?.includes({ label: 'All', value: '' })) {
+          onChange({ value: isAllSelected ? [] : allValues })
+        }
+        if (action.removedValue?.label === 'All') {
+          setVerification(false)
         }
         onChange({ value: newOptionValue })
       }}
--- src/components/form/select-group/select-group.stories.tsx
+++ src/components/form/select-group/select-group.stories.tsx
@@ -9,13 +9,50 @@ const generateOptionsFrom = (fromIndex: number) =>
     value: `${i + fromIndex}`,
   })) as SelectOption[]
 
+const testOptions = [
+  { label: 'test1', value: 'valueTest1', disabled: true },
+  { label: 'test2', value: 'valueTest2', disabled: false },
+  { label: 'test3', value: 'valueTest3', disabled: false },
+  { label: 'test4', value: 'valueTest4', disabled: false },
+  { label: 'test5', value: 'valueTest5', disabled: false },
+  { label: 'test6', value: 'valueTest6', disabled: false },
+  { label: 'test7', value: 'valueTest7', disabled: true },
+  { label: 'test8', value: 'valueTest8', disabled: true },
+]
+
+const allValues = [
+  { label: 'test1', value: 'valueTest1', disabled: true },
+  { label: 'test2', value: 'valueTest2', disabled: false },
+  { label: 'test3', value: 'valueTest3', disabled: false },
+  { label: 'test4', value: 'valueTest4', disabled: false },
+  { label: 'test5', value: 'valueTest5', disabled: false },
+  { label: 'test6', value: 'valueTest6', disabled: false },
+  { label: 'test7', value: 'valueTest7', disabled: true },
+  { label: 'test8', value: 'valueTest8', disabled: true },
+  { label: 'test9', value: 'valueTest9', disabled: true },
+  { label: 'test10', value: 'valueTest10', disabled: false },
+  { label: 'test11', value: 'valueTest11', disabled: false },
+  { label: 'test12', value: 'valueTest12', disabled: false },
+  { label: 'test13', value: 'valueTest13', disabled: false },
+  { label: 'test14', value: 'valueTest14', disabled: false },
+  { label: 'test15', value: 'valueTest15', disabled: true },
+  { label: 'test16', value: 'valueTest16', disabled: true },
+  { label: 'test17', value: 'valueTest17', disabled: true },
+  { label: 'test18', value: 'valueTest18', disabled: false },
+  { label: 'test19', value: 'valueTest19', disabled: false },
+  { label: 'test20', value: 'valueTest20', disabled: false },
+]
+
 const options = generateOptionsFrom(0)
 
 const Template: Story<Props> = (args) => {
   const [val, setValue] = useState(args.value)
+
   return (
     <SelectMulti
       {...args}
+      allValues={allValues}
+      options={testOptions}
       value={val}
       label="Select group"
       onChange={({ value }) => setValue(value)}
@@ -31,6 +68,8 @@ const InfinityScrollTemplate: Story<Props> = (args) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <SelectMulti
+        hasSelectAll
+        allValues={allValues}
         value={value}
         options={opts}
         label="Scroll me"
